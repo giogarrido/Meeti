87b6b372e768bf681e2d63be43b33a810d813b24
GARRIDO\giova <giovanni.garrido228724@potros.itson.edu.mx>
2024-04-21 13:25:20 -0700

    add sendEmail

--- handlers/email.js
+++ handlers/email.js
@@ -13,7 +13,27 @@ let transport = nodemailer.createTransport({
     }
 });
 
-exports.sendEmail = async (opciones) => {
-    console.log(opciones);
+exports.sendEmail = async (options) => {
+    console.log(options);
 
+    // Leer el archivo para el email
+    const file = __dirname + `/../views/emails/${options.file}.ejs`;
+
+    // Compilarlo
+    const compiled = ejs.compile(fs.readFileSync(file, 'utf8'));
+
+    // Crear el HTML
+    const html = compiled({ url: options.url });
+
+    // Configurar el email
+    const mailOptions = {
+        from: 'Meeti <noreply@meeti.com>',
+        to: options.user.email,
+        subject: options.subject,
+        html
+    };
+
+    // Enviar el email
+    const sendMail = util.promisify(transport.sendMail, transport);
+    return sendMail.call(transport, mailOptions);
 }
