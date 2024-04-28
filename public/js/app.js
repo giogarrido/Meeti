84dc9bf7603ae18f75d9dd0af26da512fe2aac78
GARRIDO\giova <giovanni.garrido228724@potros.itson.edu.mx>
2024-04-27 23:58:44 -0700

    add pinMaps

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ import { OpenStreetMapProvider } from 'leaflet-geosearch';
 const lat = document.querySelector('#lat').value || 27.49477;
 const lng = document.querySelector('#lng').value || -109.89998;
 const map = L.map('mapa').setView([lat, lng], 15);
+let markers = new L.FeatureGroup().addTo(map);
 let marker;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -20,23 +21,38 @@ document.addEventListener('DOMContentLoaded', () => {
 function buscarDireccion (e) {
     if(e.target.value.length > 8) {
 
-        //
+        // si existe un pin anterior limpiarlo
+        markers.clearLayers();
+
+
         const provider = new OpenStreetMapProvider();
         provider.search({ query: e.target.value })
             .then((result) => {
+                console.log(result);
                 if(result.length > 0) {
                     const { x, y } = result[0];
                     map.setView([y, x], 15);
                 
 
                 //agregar el pin
-                L.marker([y, x]).addTo(map)
-                .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+                marker = new L.marker([y, x], {
+                    draggable: true,
+                    autoPan: true
+                })
+                .addTo(map)
+                .bindPopup(result[0].label)
                 .openPopup();
+
+                //asignar al contenedor de markers
+                markers.addLayer(marker);
+
+                //
+                              
                 }
 
-            });
+            })
     }
 }
+
 
 
